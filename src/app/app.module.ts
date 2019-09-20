500180336c2e2359537335c3154cd6973fee1e2d
Danna barroso zapata <barrosozapata@gmail.com>
2019-09-19 19:27:48 -0500

    Corregiendo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app.routing';
+import { ThemeComponent } from './theme/theme.component';
+import { AdministrationComponent } from './pages/administration/administration.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ThemeComponent,
+    AdministrationComponent,
+    SettingsComponent,
+ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
